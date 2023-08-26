b7b04fd017807110fcdc9be3caab8d3daa13511b
razak2004 <abdulrazak.nasriudeen@fssa.freshworks.com>
2023-08-26 12:58:31 +0530

    created service model

--- src/main/java/com/fssa/reparo/model/Services.java
+++ src/main/java/com/fssa/reparo/model/Services.java
@@ -1,4 +1,19 @@
 package com.fssa.reparo.model;
 
-public class Services {
+public class Services extends ServicesEntities {
+    private Booking booking;
+    private WorkShop serviceWorkshop;
+    public Booking getBooking() {
+        return booking;
+    }
+    public void setBooking(Booking booking) {
+        this.booking = booking;
+    }
+    public WorkShop getServiceWorkshop() {
+        return serviceWorkshop;
+    }
+
+    public void setServiceWorkshop(WorkShop serviceWorkshop) {
+        this.serviceWorkshop = serviceWorkshop;
+    }
 }
